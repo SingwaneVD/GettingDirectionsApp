18b6ddf33728170593a5021781c2119f438fa346
SingwaneVD <94643260+SingwaneVD@users.noreply.github.com>
2022-07-27 20:45:21 +0200

    Initial Commit

---
+++ GettingDirectionsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GettingDirectionsApp
+//
+//  Created by IACD-024 on 2022/07/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
